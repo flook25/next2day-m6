29ec7885f94d32b28de0757f666ca9e940db59c5
flook25 <flook11223344@gmail.com>
2025-06-22 16:20:53 +0700

    redeploy 4

--- app/api/product/delete/[id]/route.ts
+++ app/api/product/delete/[id]/route.ts
@@ -1,6 +1,6 @@
 import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "../../../../generated/prisma";
+import { PrismaClient } from "../../../../../generated/prisma";
 
 const prisma = new PrismaClient();
 
